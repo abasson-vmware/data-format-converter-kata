ec47c53e83d927f4ffbf0f4624e836cf4303b718
Alex Basson <abasson@vmware.com>
2022-05-20 08:49:56 -0400

    Initial commit

---
+++ src/main/java/com/example/dataformatconverter/DataFormatConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.dataformatconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFormatConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataFormatConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dataformatconverter/DataFormatConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataformatconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataFormatConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
